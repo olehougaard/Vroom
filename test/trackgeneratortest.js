6e07242fd0354e7df6203d6d853e6e46ea99890d
Bruger <olehougaard@gmai.com>
2017-09-02 12:59:43 +0200

    starting and ending positions on both sides of a corner

--- test/trackgeneratortest.js
+++ test/trackgeneratortest.js
@@ -11,15 +11,36 @@ const {position, move, track, vector} = require('../track.js')
 const trackgenerator = require('../trackgenerator.js')(track, vector)
 const curve = require('../curve.js')
 
+const set_equals = ( a1, a2 ) => {
+    return a1.length === a2.length
+        && a1.every( e => a2.some( e2 => e.equals(e2)))
+}
+
 test('Straight line', expect => {
     const line = curve.straight(position(0, 3), position(10, 8))
     const width = 4
     const size = { width: 11, height: 11 }
     const { track, starting_positions } = trackgenerator.from_curve(size, line, width)
     expect.deepEquals(track.size, size, 'Obeys size')
-    expect.true(track.in_bounds(position(0, 3)))
-    expect.true(track.in_bounds(position(10, 8)))
-    expect.false(track.intersects(move(position(0, 3), vector(10, 5))), 'The original line is free')
-    expect.deepEquals(starting_positions, [position(0, 1), position(0, 2), position(0, 3), position(0, 4), position(0, 5)])
+    expect.true(track.in_bounds(position(0, 3)), 'Curve start is on track') 
+    expect.true(track.in_bounds(position(10, 8)), 'Curve end is on track')
+    expect.false(
+        track.intersects(move(position(0, 3), vector(10, 5))), 
+        'The original line is free')
+    expect.true(set_equals(
+        starting_positions, 
+        [1, 2, 3, 4, 5].map(y => position(0, y))),
+        'Starting positions along the western edge')
     expect.true(track.finish(move(position(9, 8), vector(1, 0))))
+})
+
+test('Corner to corner', expect => {
+    const line = curve.straight(position(0, 0), position(10, 10))
+    const width = 4
+    const size = { width: 11, height: 11 }
+    const { track, starting_positions } = trackgenerator.from_curve(size, line, width)
+    expect.true(set_equals(
+        starting_positions,
+        [ position(0, 2), position(0, 1), position(0, 0), position(1, 0), position(2, 0) ]),
+        'Starting positions around a corner')
 })
--- trackgenerator.js
+++ trackgenerator.js
@@ -1,19 +1,51 @@
-module.exports = (track, vector) => ({
-    from_curve(size, curve, width) {
+module.exports = (track, vector) => {
+    const edge_tile = (position, track_size) => {
+        const previous = () => {
+            let p;
+            if (position.x === 0)
+                p = position.north()
+            else if (position.y === 0)
+                p = position.west()
+            else if (position.x === track_size.width - 1)
+                p = position.south()
+            else
+                p = position.east()
+            return edge_tile(p, track_size)
+        }
+        const next = () => {
+            let p;
+            if (position.y === 0)
+                p = position.east()
+            else if (position.x === 0)
+                p = position.south()
+            else if (position.y === track_size.width - 1)
+                p = position.west()
+            else
+                p = position.north()
+            return edge_tile(p, track_size)
+        }
+        return { previous, next, position }
+    }
+    const from_curve = (size, curve, width) => {
         const outer_curve = curve.displace(vector(0, width / 2))
         const inner_curve = curve.displace(vector(0, -width / 2))
         const inbounds = p => outer_curve.is_right(p) && inner_curve.is_left(p)
         const last_free = (p, next) => {
-            while(inbounds(next(p))) p = next(p)
+            while(inbounds(next(p).position)) p = next(p)
             return p
         }
-        const take_until = (p, next) => {
+        const take_while_on_track = start => next => {
             const positions = []
-            for(; inbounds(p); p = next(p)) positions.push(p)
+            for(let p = next(start); inbounds(p.position); p = next(p)) positions.push(p)
             return positions
         }
-        const starting_positions = take_until(last_free(curve.start(), p => p.south()), p => p.north())
-        const the_track = track(size, inbounds, take_until(last_free(curve.end(), p => p.south()), p => p.north()))
-        return { track: the_track, starting_positions }
+        const directions = [ p => p.previous(), p => p.next() ]
+        const starting_positions = Array.prototype.concat.apply(
+            [ edge_tile(curve.start(), size) ],
+            directions.map ( take_while_on_track(edge_tile(curve.start(), size))))
+        const finish_line = take_while_on_track(last_free(edge_tile(curve.end(), size), p => p.previous()))(p => p.next())
+        const the_track = track(size, inbounds, finish_line.map(p => p.position))
+        return { track: the_track, starting_positions: starting_positions.map(p => p.position) }
     }
-})
+    return {from_curve}
+}
