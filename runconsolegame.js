ad2a44410455e705b04229c818ae51d5518c6942
= <=>
2017-09-10 12:52:53 +0200

    Simplifications

--- runconsolegame.js
+++ runconsolegame.js
@@ -3,31 +3,29 @@ const { run } = require('./rules.js')(vector, move)
 const { arc } = require('./curve.js')
 const { from_curve } = require('./trackgenerator.js')(track, vector)
 const consolegame = require('./consolegame.js')(run)
+const readline = require('readline').createInterface({input: process.stdin, output: process.stdout})
 
 
-const real_console = ({stdin, stdout}) => {
-    const readline = require('readline').createInterface({input: stdin, output: stdout})
-    return {
-        tell(message) {
-            readline.write(message)
-            readline.write('\n')
-        },
-        ask(question) {
-            return new Promise((resolve, reject) => {
-                try {
-                    readline.question(question + '\n', resolve)
-                } catch (e) {
-                    reject(e)
-                }
-            })
-        },
-        close() {
-            readline.close()
-        }
+const real_console = {
+    tell(message) {
+        readline.write(message)
+        readline.write('\n')
+    },
+    ask(question) {
+        return new Promise((resolve, reject) => {
+            try {
+                readline.question(question + '\n', resolve)
+            } catch (e) {
+                reject(e)
+            }
+        })
+    },
+    close() {
+        readline.close()
     }
 }
 
-const the_track = from_curve({width: 21, height: 21}, arc.negative(position(11, 0), 6, 16, Math.PI, 0), 6)
-const generator = () => the_track
+
+const generator = () => from_curve({width: 21, height: 21}, arc.negative(position(10, 0), 6, 16, Math.PI, 0), 6)
 const game = consolegame(generator)
-game.run(real_console(process))
+game.run(real_console)
